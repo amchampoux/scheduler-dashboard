33b5deae5b1f6271690e5c6e0e16bf87270c8681
amchampoux <amchampoux@mirego.com>
2023-03-22 21:58:25 -0400

    build class component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,72 @@
 import React, { Component } from "react";
-
 import classnames from "classnames";
+import Loading from "./Loading";
+import Panel from "./Panel";
+
+const data = [
+  {
+    id: 1,
+    label: "Total Interviews",
+    value: 6
+  },
+  {
+    id: 2,
+    label: "Least Popular Time Slot",
+    value: "1pm"
+  },
+  {
+    id: 3,
+    label: "Most Popular Day",
+    value: "Wednesday"
+  },
+  {
+    id: 4,
+    label: "Interviews Per Day",
+    value: "2.3"
+  }
+];
 
 class Dashboard extends Component {
-  render() {
-    const dashboardClasses = classnames("dashboard");
 
-    return <main className={dashboardClasses} />;
+  state = { 
+    loading: false,
+    focused: null
+  };
+  /* Class Property with Arrow Function */
+  // selectPanel = id => {
+  //   this.setState({
+  //    focused: id
+  //   });
+  // };
+  
+  /* Instance Method */
+  selectPanel(id) {
+    this.setState(prev => ({
+      focused: prev.focused !== null ? null : id
+    }));
+  }
+
+  render() {
+
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused
+    });
+
+    if(this.state.loading) {
+      return <Loading />;
+    }
+    
+    const panels = (this.state.focused ? data.filter(panel => this.state.focused === panel.id) : data)
+     .map(panel => (
+      <Panel 
+        key={panel.id}
+        label={panel.label}
+        value={panel.value}
+        onSelect={event => this.selectPanel(panel.id)}
+      /> 
+    ));
+
+    return <main className={dashboardClasses}>{panels}</main>;
   }
 }
 
